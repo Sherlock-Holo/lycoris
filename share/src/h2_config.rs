67385edcc00c2d002587d1d3b104d03a96cf2404
Sherlock Holo <sherlockya@gmail.com>
2022-09-16 23:03:57 +0800

    feat(share): set bigger h2 window size and frame size

--- share/src/h2_config.rs
+++ share/src/h2_config.rs
@@ -1,5 +1,6 @@
-pub const INITIAL_WINDOW_SIZE: u32 = 10 * 1024 * 1024;
-// 10 MiB
-pub const INITIAL_CONNECTION_WINDOW_SIZE: u32 = 50 * 1024 * 1024;
-// 50 MiB
-pub const MAX_FRAME_SIZE: u32 = 4 * 1024 * 1024; // 4MiB
+// 20 MiB
+pub const INITIAL_WINDOW_SIZE: u32 = 20 * 1024 * 1024;
+// 100 MiB
+pub const INITIAL_CONNECTION_WINDOW_SIZE: u32 = 100 * 1024 * 1024;
+// the h2 lib allow max size
+pub const MAX_FRAME_SIZE: u32 = 16777215;
