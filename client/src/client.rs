aa44211448f7cddff7665f5821a917171e257f33
Sherlock Holo <sherlockya@gmail.com>
2023-04-17 17:11:57 +0800

    feat(client): add connect timeout

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,8 +1,12 @@
+use std::io::{self, ErrorKind};
 use std::sync::Arc;
+use std::time::Duration;
 
 use share::proxy;
-use tracing::info;
+use tokio::time;
+use tracing::{error, info, instrument};
 
+use crate::addr::domain_or_socket_addr::DomainOrSocketAddr;
 use crate::connect::Connect;
 use crate::err::Error;
 use crate::listener::{Listener, Split};
@@ -39,17 +43,34 @@ where
 
             let connector = self.connector.clone();
 
-            tokio::spawn(async move {
-                let (read, write) = connector.connect(addr).await?;
-
-                info!("connect done");
-
-                let (tcp_read, tcp_write) = tcp_stream.into_split();
-
-                proxy::proxy(read, write, tcp_read, tcp_write).await?;
-
-                Ok::<_, Error>(())
-            });
+            tokio::spawn(handle_proxy(connector, addr, tcp_stream));
         }
     }
+}
+
+#[instrument(skip(connector, tcp_stream), err)]
+async fn handle_proxy<C: Connect, S: Split + 'static>(
+    connector: Arc<C>,
+    addr: DomainOrSocketAddr,
+    tcp_stream: S,
+) -> Result<(), Error> {
+    const CONNECT_TIMEOUT: Duration = Duration::from_secs(10);
+
+    let (read, write) = match time::timeout(CONNECT_TIMEOUT, connector.connect(addr)).await {
+        Err(err) => {
+            error!(%err, "connect timeout");
+
+            return Err(Error::Io(io::Error::new(ErrorKind::TimedOut, err)));
+        }
+
+        Ok(result) => result?,
+    };
+
+    info!("connect done");
+
+    let (tcp_read, tcp_write) = tcp_stream.into_split();
+
+    proxy::proxy(read, write, tcp_read, tcp_write).await?;
+
+    Ok::<_, Error>(())
 }
