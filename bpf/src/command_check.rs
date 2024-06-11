3081600eb64eb3ebf4432b85db58fea85072fc6e
Sherlock Holo <sherlockya@gmail.com>
2024-06-11 15:32:32 +0800

    refactor: better command str acquire

--- bpf/src/command_check.rs
+++ bpf/src/command_check.rs
@@ -1,37 +1,31 @@
-use core::ffi::{c_long, CStr};
-use core::str::from_utf8_unchecked;
+use core::ffi::c_long;
 
 use aya_ebpf::programs::SockAddrContext;
 use aya_log_ebpf::debug;
 
 use crate::map::{COMM_MAP, COMM_MAP_MODE};
-use crate::safe_helper;
+use crate::safe_helper::CommandStr;
 
 #[inline]
 pub fn command_can_connect_directly(ctx: &SockAddrContext) -> Result<bool, c_long> {
-    let mut command = [0u8; 16];
-    let null_index = safe_helper::bpf_get_current_comm(&mut command)?;
+    let command_str = CommandStr::get_command()?;
 
     // default 0 mode
     let mode = COMM_MAP_MODE.get(0).copied().unwrap_or(0);
 
     let can_connect_directly = unsafe {
-        match COMM_MAP.get(&command) {
+        match COMM_MAP.get(command_str.as_array()) {
             None => mode == 1,
             Some(_) => mode == 0,
         }
     };
 
-    let command = unsafe { CStr::from_bytes_with_nul_unchecked(&command[..null_index + 1]) };
-    let command = command.to_bytes();
-    let command = unsafe { from_utf8_unchecked(command) };
-
     if can_connect_directly {
-        debug!(ctx, "command {} can connect directly", command);
+        debug!(ctx, "command {} can connect directly", command_str);
     } else {
         debug!(
             ctx,
-            "command {} can not connect directly, need next check step", command
+            "command {} can not connect directly, need next check step", command_str
         );
     }
 
--- bpf/src/safe_helper.rs
+++ bpf/src/safe_helper.rs
@@ -1,10 +1,52 @@
-use core::ffi::c_long;
+use core::ffi::{c_long, CStr};
+use core::num::NonZeroUsize;
+use core::str::from_utf8_unchecked;
 
 use aya_ebpf::helpers::gen;
+use aya_log_ebpf::macro_support::DefaultFormatter;
+use aya_log_ebpf::WriteToBuf;
 use unroll::unroll_for_loops;
 
-/// get current comm safe helper, will return nul index
-pub fn bpf_get_current_comm(buf: &mut [u8; 16]) -> Result<usize, c_long> {
+pub struct CommandStr {
+    buf: [u8; 16],
+    index: usize,
+}
+
+impl CommandStr {
+    #[inline]
+    pub fn get_command() -> Result<Self, c_long> {
+        let mut this = Self {
+            buf: [0; 16],
+            index: 0,
+        };
+
+        let index = bpf_get_current_comm(&mut this.buf)?;
+        this.index = index;
+
+        Ok(this)
+    }
+
+    pub fn as_str(&self) -> &str {
+        let command = unsafe { CStr::from_bytes_with_nul_unchecked(&self.buf[..self.index + 1]) };
+
+        unsafe { from_utf8_unchecked(command.to_bytes()) }
+    }
+
+    pub fn as_array(&self) -> &[u8; 16] {
+        &self.buf
+    }
+}
+
+impl WriteToBuf for CommandStr {
+    fn write(self, buf: &mut [u8]) -> Option<NonZeroUsize> {
+        self.as_str().write(buf)
+    }
+}
+
+impl DefaultFormatter for CommandStr {}
+
+/// get current comm safe helper, return nul index
+fn bpf_get_current_comm(buf: &mut [u8; 16]) -> Result<usize, c_long> {
     unsafe {
         let res = gen::bpf_get_current_comm(buf.as_mut_ptr() as *mut _, buf.len() as _);
         if res < 0 {
