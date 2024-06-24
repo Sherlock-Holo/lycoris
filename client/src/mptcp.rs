59a4556c0cdf18686d7b4713cc7bd5b206226104
Sherlock Holo <sherlockya@gmail.com>
2024-06-24 15:01:59 +0800

    refactor(client): rewrite connect_mptcp implement

--- client/src/mptcp.rs
+++ client/src/mptcp.rs
@@ -1,7 +1,7 @@
 use std::ffi::c_int;
 use std::io;
 use std::io::ErrorKind;
-use std::net::{IpAddr, SocketAddr, TcpStream as StdTcpStream};
+use std::net::{SocketAddr, TcpStream as StdTcpStream};
 use std::pin::pin;
 
 use futures_util::{Stream, StreamExt};
@@ -20,14 +20,10 @@ impl MptcpExt for TcpStream {
     where
         Self: Sized,
     {
-        let ty = Type::from(SOCK_NONBLOCK | c_int::from(Type::STREAM));
-        let socket = Socket::new(Domain::IPV6, ty, Some(Protocol::MPTCP))?;
-
         let mut addrs = pin!(addrs);
-        let mut connect_success = false;
         let mut last_err = None;
         while let Some(addr) = addrs.next().await {
-            let mut addr = match addr {
+            let addr = match addr {
                 Err(err) => {
                     last_err = Some(err);
 
@@ -37,45 +33,47 @@ impl MptcpExt for TcpStream {
                 Ok(addr) => addr,
             };
 
-            if let IpAddr::V4(ip) = addr.ip() {
-                let ip = ip.to_ipv6_mapped();
-                addr.set_ip(ip.into());
-            }
-            let sock_addr = SockAddr::from(addr);
-            match socket.connect(&sock_addr) {
-                Err(err) if err.kind() == ErrorKind::WouldBlock => {
-                    connect_success = true;
-                }
+            match connect_mptcp_addr(addr).await {
                 Err(err) => {
-                    if let Some(raw_err) = err.raw_os_error() {
-                        if raw_err == EINPROGRESS {
-                            connect_success = true;
-
-                            continue;
-                        }
-                    }
-
                     last_err = Some(err);
                 }
 
-                Ok(_) => {
-                    connect_success = true;
-                    last_err = None;
-                }
+                Ok(stream) => return Ok(stream),
             }
         }
 
-        if let Some(err) = last_err {
-            // ignore error when connect is success
-            if !connect_success {
+        Err(last_err.unwrap_or_else(|| io::Error::new(ErrorKind::Other, "addrs is empty")))
+    }
+}
+
+async fn connect_mptcp_addr(addr: SocketAddr) -> io::Result<TcpStream> {
+    let domain = match addr {
+        SocketAddr::V4(_) => Domain::IPV4,
+        SocketAddr::V6(_) => Domain::IPV6,
+    };
+
+    let ty = Type::from(SOCK_NONBLOCK | c_int::from(Type::STREAM));
+    let socket = Socket::new(domain, ty, Some(Protocol::MPTCP))?;
+
+    let sock_addr = SockAddr::from(addr);
+    match socket.connect(&sock_addr) {
+        Err(err) if err.kind() == ErrorKind::WouldBlock => {}
+        Err(err) => {
+            if let Some(raw_err) = err.raw_os_error() {
+                if raw_err != EINPROGRESS {
+                    return Err(err);
+                }
+            } else {
                 return Err(err);
             }
         }
 
-        let std_tcp_stream = StdTcpStream::from(socket);
-        let tcp_stream = TcpStream::from_std(std_tcp_stream)?;
-        tcp_stream.writable().await?;
-
-        Ok(tcp_stream)
+        Ok(_) => {}
     }
+
+    let std_tcp_stream = StdTcpStream::from(socket);
+    let tcp_stream = TcpStream::from_std(std_tcp_stream)?;
+    tcp_stream.writable().await?;
+
+    Ok(tcp_stream)
 }
